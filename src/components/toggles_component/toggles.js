3aa10c9d276f0c60e3f1712e671fa042c74d822f
Selim <selimabdelmonsef@gmail.com>
2021-06-09 00:41:26 +0200

    implemented toggle page, handled web design

--- src/components/toggles_component/toggles.js
+++ src/components/toggles_component/toggles.js
@@ -1,32 +1,42 @@
 import React from 'react'
 import styles from './toggles.module.css'
-import { Form} from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Dropdown from 'react-bootstrap/Dropdown'
+import { BsSearch } from 'react-icons/all'
 
 export default class TogglingForm extends React.Component {
 
     render() {
         return (
-            <div className={styles.togglesFormStyle}>
-              <Form>
-  <Form.Group>
-    <Form.Label>JOB</Form.Label>
-    <Form.Control placeholder="job title, keywords" />
-  </Form.Group>
+            <div>
+                <Form className={styles.togglesFormStyle}>
+                    <div>
+                        <BsSearch className={styles.searchIcon}/>
+                    <input placeholder="Start Searching"></input>
+                    <hr/>
+                    </div>  
+                    <Form.Group>
+                        <Form.Label>JOB</Form.Label>
+                        <Form.Control placeholder="job title, keywords" />
+                    </Form.Group>
 
-  <Form.Group>
-    <Form.Label>LOCATION</Form.Label>
-    <Form.Control placeholder="City, province or region" />
-  </Form.Group>
- <div>CATEGORY</div>
-  <select>
-  <option selected>Select Industry</option>
-  <option >Engineering</option>
-  <option>Business</option>
-  <option>Designer</option>
-</select>
-</Form>
+                    <Form.Group>
+                        <Form.Label>LOCATION</Form.Label>
+                        <Form.Control placeholder="City, province or region" />
+                    </Form.Group>
+                    <div>CATEGORY</div>
+                    <select>
+                        <option selected>Select Industry</option>
+                        <option >Engineering</option>
+                        <option>Business</option>
+                        <option>Designer</option>
+                    </select>
+                    <div>
+                    <button className={styles.searchBtn}>
+                    <BsSearch className={styles.searchIcon}/> Search</button>
+                    </div>
+                    
+                </Form>
 
 
             </div>
