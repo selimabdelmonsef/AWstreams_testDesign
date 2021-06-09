d28a5db0d9acfbed868488b6a126d498a0f42d0c
Selim <selimabdelmonsef@gmail.com>
2021-06-09 18:03:36 +0200

    fixed web design

--- src/components/candidatesCarousel_component/candidatesCarousel.js
+++ src/components/candidatesCarousel_component/candidatesCarousel.js
@@ -5,19 +5,19 @@ import {candidate} from '../../constants/candidates_constant';
 export default class CandidatesCarousel extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.candiCarouselStyle}>
                 <p className={styles.candidatesTitle}>Explore Our Latest Candidates</p>
             <div className={styles.candidateStyle}>
                 
             <Splide options={ {
                 perMove:1,
-                // autoWidth:true,
-                // autoHeight:true,
+                autoWidth:true,
+                autoHeight:true,
                 rewind: true,
               } }>
                   {candidate.map((element)=>{
                       return <SplideSlide>
-                      <img src={element.img} alt = ""/>
+                      <img className={styles.cadiImage} src={element.img} alt = ""/>
                       <div className={styles.candidateName}>
               <p>{element.name}</p>
               <p className={styles.jobTitleStyle}>{element.jobTitle}</p>
--- src/pages/landing_page/landing_page.js
+++ src/pages/landing_page/landing_page.js
@@ -3,14 +3,19 @@ import FindJobTabs from '../../components/tabs_copmonent/tabs';
 import TogglingForm from '../../components/toggles_component/toggles';
 import FeatureJobSlider from '../../components/jobSlider_component/jobSlider';
 import CandidatesCarousel from '../../components/candidatesCarousel_component/candidatesCarousel';
+import styles from './landing_page.module.css'
 export default class LandingPage extends React.Component {
 
     render(){
         return(
          <div>
+             <div className={styles.tabsTogglingWrap}>
              <FindJobTabs></FindJobTabs>
+
              <TogglingForm></TogglingForm>
              <FeatureJobSlider></FeatureJobSlider>
+             </div>
+             
              <CandidatesCarousel></CandidatesCarousel>
              </div>
         )
